6bf29ab938e3ee7c4ec3016b8c3b2ec2988c92f4
James Olivas <jamesbolivas@gmail.com>
2020-08-31 15:25:39 -0700

    Add comments indicating the commands to use for future reference

--- server/cassandraSeed.js
+++ server/cassandraSeed.js
@@ -13,3 +13,18 @@ client.connect(function (err) {
   if (err) throw err;
   console.log('Connected to client');
 });
+
+// `CREATE KEYSPACE carousel
+//   WITH REPLICATION = {
+//     'class' : 'SimpleStrategy',
+//       'replication_factor' : 1
+//    };`;
+// `CREATE TABLE carousel.listings(
+//    listing_id int PRIMARY KEY,
+//    imageUrls set<text>
+//  )`;
+// COPY carousel.listings (listing_id, imageUrls)
+//   FROM '/Users/jamesolivas/Downloads/hrr47/system-design-capstone/carousel-service/data.csv'
+//   WITH HEADER = FALSE;
+
+seed();
