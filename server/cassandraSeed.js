73bd010692a3a22a7c4f75964365677a29af1e35
James Olivas <jamesbolivas@gmail.com>
2020-08-31 16:03:47 -0700

    Move out commented code, rename data-gen function

--- server/cassandraSeed.js
+++ server/cassandraSeed.js
@@ -4,24 +4,7 @@ const client = new cassandra.Client({
   contactPoints: ['127.0.0.1'], localDataCenter: 'datacenter1',
 });
 
-// const query = 'SELECT name, email FROM users WHERE key = ?';
-// client.execute(query, [ 'someone' ])
-//   .then(result => console.log('User with email %s', result.rows[0].email));
-
 client.connect((err) => {
   if (err) throw err;
   console.log('Connected to client');
 });
-
-// `CREATE KEYSPACE carousel
-//   WITH REPLICATION = {
-//     'class' : 'SimpleStrategy',
-//       'replication_factor' : 1
-//    };`;
-// `CREATE TABLE carousel.listings(
-//    listing_id int PRIMARY KEY,
-//    imageUrls set<text>
-//  )`;
-// COPY carousel.listings (listing_id, imageUrls)
-//   FROM '/Users/jamesolivas/Downloads/hrr47/system-design-capstone/carousel-service/data.csv'
-//   WITH HEADER = FALSE;
--- server/csvgen.js
+++ server/csvgen.js
@@ -6,7 +6,6 @@ let start;
 let seconds;
 
 const insert = (listingId, imageUrls) => new Promise((resolve, reject) => {
-  // const line = ''.concat(listingId, ',"', imageUrls.join(', '), '"\n');
   const line = ''.concat(listingId, ',"{', imageUrls.join(', '), '}"\n');
   fs.appendFile('data.csv', line, (err, success) => {
     if (err) {
@@ -17,7 +16,7 @@ const insert = (listingId, imageUrls) => new Promise((resolve, reject) => {
   });
 });
 
-const gen5000 = (iteration = 0) => {
+const genFiveThousand = (iteration = 0) => {
   const allPromises = [];
   let primary = 1;
   while (primary <= 5000) {
@@ -36,21 +35,20 @@ const gen5000 = (iteration = 0) => {
         if (iteration % 1000000 === 0 && iteration > 0) {
           console.log(`${iteration} seeded!`);
         }
-        gen5000(iteration + 5000);
+        genFiveThousand(iteration + 5000);
       } else {
         seconds = Math.floor((new Date().getTime() - start) / 1000);
         console.log(`Done in ${seconds} seconds!`);
       }
     })
     .catch((error) => console.log(error));
-  // });
 };
 
 const gen = () => {
   fs.writeFile('data.csv', '', (err) => {
     if (err) throw err;
     start = new Date().getTime();
-    gen5000();
+    genFiveThousand();
   });
 };
 
