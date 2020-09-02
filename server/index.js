dd2588f075cbc2130634fb89bc3d801a21c77e57
James Olivas <jamesbolivas@gmail.com>
2020-09-02 12:16:06 -0700

    Update front-end GET and POST to use Postgres

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const db = require('../db');
-const { seedOne } = require('./datagen');
+const client = require('../db/postgres.index.js');
 
 const app = express();
 
@@ -17,28 +18,42 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/images/:id', (req, res) => {
-  const id = req.url.slice(12);
-  db.query('SELECT * FROM images WHERE location_id = ? ORDER BY img_order ASC', [id], (err, data) => {
+  const id = req.params.id;
+  client.query('SELECT * FROM listings WHERE listing_id = $1', [id], (err, data) => {
     if (err) {
       res.send('An error occurred');
     } else {
-      res.send(data);
+      const location_id = data.rows[0].listing_id;
+      const urls = data.rows[0].imageurls;
+      const sculptedData = urls.map((url, i) => {
+        return {
+          img_order: i + 1,
+          location_id,
+          url
+        };
+      });
+      res.send(sculptedData);
     }
-  });
+  })
 });
 
 // POST adds images with a new location_id
 app.post('/api/images/', (req, res) => {
-  db.query('SELECT MAX(location_id) FROM images', (err, data) => {
-    if (err) {
-      res.send('An error occurred');
-    } else {
-      const destination = data[0]['MAX(location_id)'] + 1;
-      seedOne(destination, () => {
-        res.send(`Created listing ${destination}`);
-      });
+    const totalImages = Math.floor(Math.random() * (25)) + 5;
+    const imageUrls = [];
+    for (let i = 1; i < totalImages; i += 1) {
+      const img = Math.ceil(Math.random() * 369);
+      imageUrls.push(`https://hrr47-sdc-burke-carousel-images.s3.us-west-1.amazonaws.com/img${img}.jpg`);
     }
-  });
+    let urlString = '"';
+    urlString = urlString.concat(imageUrls.join('", "'), '"');
+    client.query(`INSERT INTO listings (imageUrls) VALUES ('{${urlString}}')`, (err, data) => {
+      if (err) {
+        res.send('An error occurred');
+      } else {
+        res.sendStatus(200);
+      }
+    });
 });
 
 // PUT adds extra images with the given location_id
