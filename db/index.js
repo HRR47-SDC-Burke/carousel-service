226ead571cd8d7886e91ec77d7147ff660bb33cd
Ubuntu <ubuntu@ip-172-31-3-255.us-west-1.compute.internal>
2020-09-09 18:11:49 +0000

    Update files to connect to remote database

--- db/index.js
+++ db/index.js
@@ -3,6 +3,8 @@ const { Client } = require('pg');
 
 const client = new Client({
   user: process.env.PG_USER,
+  password: process.env.PG_PWD,
+  host: process.env.PG_HOST,
   database: 'carousel',
   port: process.env.PG_PORT,
 });
